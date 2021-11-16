0d76b950a7378ad107acc8c73b5b54003358204d
restufff <restuff666@gmail.com>
2021-11-16 14:56:39 +0700

    add deployement setting

---
+++ .history/config/env/development/database_20211116124433.js
@@ -0,0 +1,14 @@
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "mongoose",
+      settings: {
+        uri: env("DATABASE_URI"),
+        srv: env.bool("DATABASE_SRV", true),
+        port: env.int("DATABASE_PORT", 27017),
+        database: env("DATABASE_NAME"),
+      },
+    },
+  },
+});
