c717e662561e3bce316343ed919057a618b07766
restufff <restuff666@gmail.com>
2021-11-16 11:36:33 +0700

    hadeh

--- .history/config/env/production/database_20211116113244.js
+++ .history/config/env/production/database_20211116113244.js
@@ -11,7 +11,7 @@ module.exports = ({ env }) => ({
       },
       options: {
         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
-        ssl: env.bool("DATABASE_SSL", true),
+        ssl: env.bool("DATABASE_SSL", false),
       },
     },
   },
